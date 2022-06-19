ef29e27b9c8fc08783407fef3c640502ef7fc48c
Rellyson Silva <rellyson@proton.me>
2022-06-19 15:25:13 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CryptoCoin PWA - MObile Applications (PUC Minas)
---
+++ service-worker.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const CACHE_NAME = "cryptocoin_static";
+
+const FILES_CACHE = [
+    'css/animate.min.css',
+    'css/bootstrap.min.css',
+    'css/main.css',
+    'images/logo.png',
+    'js/bootstrap.bundle.min.js',
+    'fonts/Poppins-Regular.ttf',
+    'offline.html',
+    'js/main.js',
+    'service-worker.js',
+    'manifest.json',
+    'api/criptocurrency-list.json',
+    'api/criptocurrency.json',
+];
+
+//Install and activate progressive web app
+self.addEventListener('activate', (evt) => {
+
+    evt.waitUntil(
+
+        caches.keys().then((keylist) => {
+
+            return Promise.all(keylist.map((key) => {
+
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+
+            }));
+
+        })
+    )
+});
+
+self.addEventListener('install', (evt) => {
+
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("[Service Worker] - Registering static cache");
+            return cache.addAll(FILES_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+
+});
+
+//Enabling offline navigation
+self.addEventListener('fetch', (evt) => {
+
+    if (evt.request.mode !== 'navigate') {
+        return;
+    }
+
+    evt.respondWith(
+
+        fetch(evt.request).catch(() => {
+
+            return caches.open(CACHE_NAME).then((cache) => {
+
+                return cache.match('offline.html');
+
+            });
+
+        })
+
+    );
+
+});
